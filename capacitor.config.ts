7e76463813795b7e5ed9cef1e16581e14f7aa957
Gil Marques <gilmmm4@gmail.com>
2026-09-06 16:59:21 +0100

    chore: generate Android app icon and splash from resources/icon.png

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Emaki',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    CapacitorAssets: {
+      iconBackgroundColor: '#4C5A60',
+      iconBackgroundColorDark: '#39434A',
+    },
+  },
 };
 
 export default config;
